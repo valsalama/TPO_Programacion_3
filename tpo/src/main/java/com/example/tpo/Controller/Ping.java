32fbeb567e5e848e55cd1bbeedc95e42ab99db7b
Lola Escalas <lolaescalaspralong@gmail.com>
2025-10-08 09:19:33 -0300

    Agrego carpeta tpo

---
+++ tpo/src/main/java/com/example/tpo/Controller/Ping.java
@@ -0,0 +1,3 @@
+public class Ping {
+    return "Pong";
+}
---
+++ tpo/src/main/java/com/example/tpo/TpoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpoApplication.class, args);
+	}
+
+}
