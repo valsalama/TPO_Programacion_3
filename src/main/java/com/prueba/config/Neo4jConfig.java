a9d90659ddb13a7f5c9cb4195cd723eb48bbc5c1
Lola Escalas <lolaescalaspralong@gmail.com>
2025-10-29 08:40:15 -0300

    Actualización del proyecto

--- src/main/java/com/prueba/config/Neo4jConfig.java
+++ src/main/java/com/prueba/config/Neo4jConfig.java
@@ -1,12 +1,24 @@
 package com.prueba.config;
 
+import org.neo4j.driver.Driver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.core.DatabaseSelectionProvider;
+import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
 
 @Configuration
 public class Neo4jConfig {
+
+    private final Driver driver;
+    private final DatabaseSelectionProvider databaseSelectionProvider;
+
+    public Neo4jConfig(Driver driver, DatabaseSelectionProvider databaseSelectionProvider) {
+        this.driver = driver;
+        this.databaseSelectionProvider = databaseSelectionProvider;
+    }
+
     @Bean
-    public ReactiveNeo4jTransactionManager reactiveTransactionManager(Driver driver, ReactiveDatabaseSelectionProvider databaseSelectionProvider) {
- return new ReactiveNeo4jTransactionManager(driver, databaseSelectionProvider);
- }
+    public Neo4jTransactionManager transactionManager() {
+        return new Neo4jTransactionManager(driver, databaseSelectionProvider);
+    }
 }
---
+++ src/main/java/com/prueba/modelo/Barrio.java
@@ -0,0 +1,15 @@
+package com.prueba.modelo;
+
+import java.sql.Date;
+import java.util.List;
+
+public class Barrio {
+
+    private int id;
+    private String nombre;
+    private double superficie;
+    private Date aniversario;
+    private List<Barrio> limites;
+
+    
+}
