c58df7cb25d08186523e53317be74bc912639526
sofidar <daruwu475@gmail.com>
2025-10-29 09:43:00 -0300

    Actualización del proyecto

--- src/main/java/com/prueba/config/Neo4jConfig.java
+++ src/main/java/com/prueba/config/Neo4jConfig.java
@@ -1,12 +1,9 @@
 package com.prueba.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Neo4jConfig {
-    @Bean
-    public ReactiveNeo4jTransactionManager reactiveTransactionManager(Driver driver, ReactiveDatabaseSelectionProvider databaseSelectionProvider) {
- return new ReactiveNeo4jTransactionManager(driver, databaseSelectionProvider);
+
  }
-}
+
---
+++ src/main/java/com/prueba/model/Arista.java
@@ -0,0 +1,11 @@
+package com.prueba.model;
+
+public class Arista {
+    public String destino;
+    public double peso;
+
+    public Arista(String destino, double peso) {
+        this.destino = destino;
+        this.peso = peso;
+    }
+}
